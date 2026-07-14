4adcf2aedd862972a4fa7028e2d2ced254724450
Estefania <estefania.cruz.mu@usb.edu.mx>
2026-07-14 00:14:59 -0600

    Update model path in Simulador3D.js

--- js/Simulador3D.js
+++ js/Simulador3D.js
@@ -69,7 +69,7 @@ function init() {
     scene.add(directionalLight);
 
     // Carga del modelo nuevo
-    const rutaModelo = '../model/the_design_lab.glb?v=1';
+    const rutaModelo = '../model/escenario.glb?v=99';
     const loader = new GLTFLoader();
 
     console.log("Intentando cargar el modelo desde la ruta:", rutaModelo);
